aac38a9cf1c01c7a6649b0992244910ed532445d
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-03-18 17:16:42 +0100

    Added people to console output

--- Source/MyConsoleApp/Program.cs
+++ Source/MyConsoleApp/Program.cs
@@ -14,9 +14,23 @@ namespace MyConsoleApp
             StarshipResponse starshipResponse = await new StarshipResponse().GetShips();
             PeopleResponse peopleResponse = await new PeopleResponse().GetPeople();
 
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("All spaceships:");
+            Console.ResetColor();
+            Console.WriteLine();
             foreach (var p in starshipResponse.Results)
             {
                 Console.WriteLine(p.Name);
+            }
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("All people:");
+            Console.ResetColor();
+            Console.WriteLine();
+            foreach (var item in peopleResponse.Results)
+            {
+                Console.WriteLine(item.Name);
             }
 
             Console.ReadKey();
