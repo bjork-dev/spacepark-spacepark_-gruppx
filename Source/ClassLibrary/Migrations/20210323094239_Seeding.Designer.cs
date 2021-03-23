452e110d1b01393a807692f8b0887ea66d7710d6
Svetlana Ernyleva <70198472+SvetlanaErn@users.noreply.github.com>
2021-03-23 10:50:16 +0100

    Adds parking spaces to database

--- Source/ClassLibrary/Migrations/20210323094239_Seeding.Designer.cs
+++ Source/ClassLibrary/Migrations/20210323094239_Seeding.Designer.cs
@@ -4,14 +4,16 @@ using ClassLibrary;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ClassLibrary.Migrations
 {
     [DbContext(typeof(SpaceContext))]
-    partial class SpaceContextModelSnapshot : ModelSnapshot
+    [Migration("20210323094239_Seeding")]
+    partial class Seeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Source/ClassLibrary/Migrations/20210323094239_Seeding.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClassLibrary.Migrations
+{
+    public partial class Seeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Parkings",
+                columns: new[] { "Id", "Fee", "MaxLength", "Occupied", "User" },
+                values: new object[,]
+                {
+                    { 1, 10, 50m, false, null },
+                    { 2, 50, 100m, false, null },
+                    { 3, 100, 200m, false, null },
+                    { 4, 1000, 2000m, false, null },
+                    { 5, 5, 15m, false, null }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parkings",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Parkings",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Parkings",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Parkings",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Parkings",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
